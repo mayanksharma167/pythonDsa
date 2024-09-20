19adc161af5d7e111c98d98618bf296c92bb721e
Mayank Sharma <mayankofficial167@gmail.com>
2024-09-20 18:45:14 +0530

    done

--- 1 - Pattern problems/4- character_pattern (IMP).py
+++ 1 - Pattern problems/4- character_pattern (IMP).py
@@ -4,6 +4,10 @@ B C
 D E F
 G H I J
 
+
+ord('A') converts the character 'A' to its ASCII value (65).
+chr(char) converts the ASCII value back to the corresponding character.
+Each time, we increment the char value by 1 to get the next character in the alphabet.
 """
 
 def char_pattern(n):
